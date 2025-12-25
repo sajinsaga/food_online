f190176a819f04f0973990a0199a6a3f6e7f3a79
Mir Sajin <sajinmahmud219@gmail.com>
2025-12-25 14:53:32 +0600

    missing one from2nd

--- foodonline_main/urls.py
+++ foodonline_main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import views # Correct: The dot means "look in the current folder"
+from . import views 
 from django.contrib import admin
 from django.urls import path
 
